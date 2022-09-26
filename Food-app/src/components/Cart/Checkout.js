66c3e9e92b59a10dccf1d375cad03ddcf41293ce
Sunny00710 <h3021307@gmail.com>
2022-09-26 17:30:17 +0900

    commit

---
+++ Food-app/src/components/Cart/Checkout.js
@@ -0,0 +1,11 @@
+import classes from "./Checkout.module.css";
+
+const checkout = (props) => {
+  return (
+    <form>
+      <div className={classes.control}></div>
+    </form>
+  );
+};
+
+export default checkout;
